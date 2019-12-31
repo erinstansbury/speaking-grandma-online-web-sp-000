90d02af6c0e549aa290ed25a945a18c18e6b3367
Erin Stansbury <ekstansbury@gmail.com>
2019-12-31 19:12:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,10 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-  else string != string.upcase
+  elsif string != string.upcase
       return "HUH?! SPEAK UP, SONNY!"
+  else string == string.upcase
+      return "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
